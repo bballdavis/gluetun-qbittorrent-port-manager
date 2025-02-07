b183143f67aefb2722cc98dd571a02c9460fa044
philipdavis <philip@davisho.me>
2025-02-07 02:21:40 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ update_port () {
   # Log in to the qbittorrent web UI and save cookies
   curl -s -c "$COOKIES" --data "username=$QBITTORRENT_USER&password=$QBITTORRENT_PASS" "${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2/auth/login" > /dev/null
   if [[ $? -ne 0 ]]; then
-    echo "[Error] Login failed."
+    echo "[Error]❌ Login failed."
     return 1
   fi
 
@@ -24,16 +24,37 @@ update_port () {
 
   if [ "$CURRENT_PORT" == "$PORT" ]; then
     echo "[Info] Successfully updated qbittorrent to port $PORT"
+    reannounce_all "$COOKIES"
     return 0
   else
-    echo "[Error] Failed to update port."
+    echo "[Error]❌ Failed to update port."
     return 1
   fi
 
   # Clean up cookies file
   rm -f "$COOKIES"
 
-  echo "[Info] Successfully updated qbittorrent to port $PORT"
+  echo "[Info]✅ Successfully updated qbittorrent to port $PORT"
+}
+
+reannounce_all () {
+# Get list of all torrent hashes
+  TORRENTS_JSON=$(curl -s -b "$COOKIEs" "${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2/torrents/info")
+  HASHES=$(echo "$TORRENTS_JSON" | jq -r '.[].hash')
+
+  if [[ -z "$HASHES" ]]; then
+      echo "❌ No torrents found!"
+      exit 1
+  fi
+
+  echo "📂 Found $(echo "$HASHES" | wc -l) torrents. Reannouncing..."
+
+  # Reannounce each torrent
+  curl -X POST "${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2/torrents/reannounce" \
+      --data "hashes=all" \
+      -b "$COOKIE_JAR"
+
+echo "✅ All torrents reannounced!"
 }
 
 # Main loop to check the port and update if necessary
@@ -43,7 +64,7 @@ while true; do
   
   # Check if the fetched port is valid
   if [[ -z "$PORT_FORWARDED" || ! "$PORT_FORWARDED" =~ ^[0-9]+$ ]]; then
-    echo "[Error] Failed to retrieve a valid port number, response from Gluetun.D"
+    echo "[Error]❌ Failed to retrieve a valid port number, response from Gluetun."
     sleep 10
     continue
   fi
@@ -52,7 +73,7 @@ while true; do
   if [[ "$CURRENT_PORT" != "$PORT_FORWARDED" ]]; then
     update_port "$PORT_FORWARDED"
   else
-    echo "[Info] Current Gluetun port matches Qbittorrent port."
+    echo "[Info]🟰 Current Gluetun port matches Qbittorrent port."
   fi
 
   # Wait for a specific interval before checking again
